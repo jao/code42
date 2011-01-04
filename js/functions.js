3d982db923e2fd071c3fd962e87381b4121c8431
joao paulo <jpcamargo+github@gmail.com>
2011-01-04 13:05:42 -0200

    changes to css, html and js files - added a readme file

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,13 @@
-$(document).ready(function(){
-  $("#pageflip").hover(function() { //On hover...
-  	$("#pageflip img , .msg_block").stop()
-  		.animate({ //Animate and expand the image and the msg_block (Width + height)
-  			width: '90px', //307
-  			height: '90px' //319
-  		}, 500);
-  	} , function() {
-  	$("#pageflip img").stop() //On hover out, go back to original size 50x52
-  		.animate({
-  			width: '50px',
-  			height: '52px'
-  		}, 220);
-  	$(".msg_block").stop() //On hover out, go back to original size 50x50
-  		.animate({
-  			width: '50px',
-  			height: '50px'
-  		}, 200); //Note this one retracts a bit faster (to prevent glitching in IE)
-  });
+$(function(){
+   //On hover...
+  $("#pageflip").hover(
+    //Animate and expand the image and the msg_block (Width + height)
+    function(){
+      $("#pageflip img, .message").stop().animate({width:'90px', height:'90px'}, 500);
+    },
+    //On hover out, go back to original size 50x52
+    function(){
+      $("#pageflip img").stop().animate({width:'50px', height:'52px'}, 220);
+      $(".message").stop().animate({width:'50px', height:'50px'}, 200); //Note this one retracts a bit faster (to prevent glitching in IE)
+    });
 });
